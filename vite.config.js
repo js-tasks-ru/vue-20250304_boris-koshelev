7e27b7429aded8f75aace7e2becf99f5b9551d9e
bkoshelev <brskoshelev@gmail.com>
2025-03-08 19:25:58 +0300

    add solution for 10-sum

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import { createTaskbookViteConfig } from '@shgk/vue-course-taskbook/configs/vite.config.js'
 
 const customConfig = defineConfig({
+  test: {
+    include: ['./00-intro/**/__tests__/*'],
+  },
   // Здесь вы можете переопределять конфигурацию Vite
 })
 
