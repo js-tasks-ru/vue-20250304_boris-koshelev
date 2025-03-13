d7dcd61d6db4c0a3f55cc9a72c6439ce27e2995b
bkoshelev <brskoshelev@gmail.com>
2025-03-13 23:14:30 +0300

    add solution for 40-UiCounter

--- 03-components/40-UiCounter/UiCounter.js
+++ 03-components/40-UiCounter/UiCounter.js
@@ -1,4 +1,4 @@
-import { defineComponent } from 'vue'
+import { computed, defineComponent } from 'vue'
 import { UiButton } from '@shgk/vue-course-ui'
 import './UiCounter.css'
 
@@ -9,15 +9,46 @@ export default defineComponent({
     UiButton,
   },
 
-  setup() {
+  props: {
+    count: Number,
+    min: {
+      type: Number,
+      default: 0,
+    },
+
+    max: {
+      type: Number,
+      default: Infinity,
+    },
+  },
+
+  emits: ['update:count'],
+
+  setup(props, ctx) {
+    function decrement() {
+      ctx.emit('update:count', props.count - 1)
+    }
+    function increment() {
+      ctx.emit('update:count', props.count + 1)
+    }
+
+    const decrementButtonDisabled = computed(() => props.count <= props.min)
+    const incrementButtonDisabled = computed(() => props.count >= props.max)
+
+    return {
+      decrement,
+      increment,
+      decrementButtonDisabled,
+      incrementButtonDisabled,
+    }
     // Рекомендуется для практики реализовать обработку событий внутри setup, а не непосредственно в шаблоне
   },
 
   template: `
     <div class="counter">
-      <UiButton aria-label="Decrement" disabled>➖</UiButton>
-      <span class="count" data-testid="count">3</span>
-      <UiButton aria-label="Increment">➕</UiButton>
+      <UiButton aria-label="Decrement" @click="decrement" :disabled="decrementButtonDisabled">➖</UiButton>
+      <span class="count" data-testid="count" >{{ count }}</span>
+      <UiButton aria-label="Increment" @click="increment" :disabled="incrementButtonDisabled">➕</UiButton>
     </div>
   `,
 })
