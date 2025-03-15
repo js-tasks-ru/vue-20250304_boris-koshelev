d776437efa95c082d240b80cc69ae06c52e9ce30
bkoshelev <brskoshelev@gmail.com>
2025-03-15 16:55:41 +0300

    add solution for 20-MeetupCover-style-v-bind

--- 04-sfc/20-MeetupCover-style-v-bind/MeetupCover.vue
+++ 04-sfc/20-MeetupCover-style-v-bind/MeetupCover.vue
@@ -11,11 +11,11 @@ const props = defineProps({
   },
 })
 
-const bgStyle = computed(() => (props.image ? { '--bg-url': `url('${props.image}')` } : undefined))
+const bgUrl = computed(() => `url(${props.image})`)
 </script>
 
 <template>
-  <div class="meetup-cover" :style="bgStyle">
+  <div class="meetup-cover">
     <h1 class="meetup-cover__title">{{ title }}</h1>
   </div>
 </template>
@@ -26,7 +26,8 @@ const bgStyle = computed(() => (props.image ? { '--bg-url': `url('${props.image}
   background-position: center;
   /* Если изображение присутствует - берём его из CSS переменной, установленной на элемент в шаблоне */
   /* Иначе выводим изображение по умолчанию - var(--default-cover) */
-  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), var(--bg-url, var(--default-cover));
+  --bgUrl: v-bind('bgUrl'), var(--default-cover);
+  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), var(--bgUrl);
   display: flex;
   flex-direction: column;
   align-items: center;
